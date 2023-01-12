629f02dbde227e793700958d718caa8fb5dd261d
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-12 13:31:31 +0300

    added if the message is a string

--- fuunction3.py
+++ fuunction3.py
@@ -1,24 +1,27 @@
 
-def myString(message:str):
-    count = 0
-    for check in range(len(message)):
-        check2 = message[check]
-        if check2.isupper():
-            result = ""
-            for letter in range(len(message)):
-                letter2 = message[letter]
-                if letter2.isupper():
-                    result = result +" "+ letter2.lower()
-                else:
-                    result = result + letter2
-            print(result)        
+def myString(message):
+    if type(message) == str:
+        count = 0
+        for check in range(len(message)):
+            check2 = message[check]
+            if check2.isupper():
+                result = ""
+                for letter in range(len(message)):
+                    letter2 = message[letter]
+                    if letter2.isupper():
+                        result = result +" "+ letter2.lower()
+                    else:
+                        result = result + letter2
+                print(result)        
+            else:
+                count = count + 1
+                continue
+        if count == len(message):
+            print("ther is no capital letters")
         else:
-            count = count + 1
-            continue
-    if count == len(message):
-        print("ther is no capital letters")
+            return
     else:
-        return
+        print("the message is not valid")
 
 message = input("Enter a message: ")
 myString(message)
