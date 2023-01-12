5536967d211db1541f0c12db9d7087a20a4e5f9e
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-12 11:59:44 +0300

    made it a function

--- fuunction3.py
+++ fuunction3.py
@@ -1,21 +1,24 @@
-message = input("Enter a message: ")
 
-for check in range(len(message)):
+def myString(message:str):
     count = 0
-    check2 = message[check]
-    if check2.isupper():
-        result = ""
-        for letter in range(len(message)):
-            letter2 = message[letter]
-            if letter2.isupper():
-                result = result +" "+ letter2.lower()
-            else:
-                result = result + letter2
-        print(result)        
-    else:
-        count += 1
-        continue
+    for check in range(len(message)):
+        check2 = message[check]
+        if check2.isupper():
+            result = ""
+            for letter in range(len(message)):
+                letter2 = message[letter]
+                if letter2.isupper():
+                    result = result +" "+ letter2.lower()
+                else:
+                    result = result + letter2
+            print(result)        
+        else:
+            count = count + 1
+            continue
     if count == len(message):
         print("ther is no capital letters")
     else:
-        break
+        return
+
+message = input("Enter a message: ")
+myString(message)
