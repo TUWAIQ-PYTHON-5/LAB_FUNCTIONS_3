e622918b045f7c558222a2c941d4735dbdbc5255
halrasheed <haroon.arrsheed@gmail.com>
2023-01-12 12:17:28 +0300

    Function3 Lab upload

---
+++ Function3Lab.py
@@ -0,0 +1,28 @@
+'''
+# LAB_FUNCTIONS_3
+
+## write a function that takes a string as a parameter
+- first check that the type of the parameter is of type str
+- then, it should separates the word at any capital letter and replace it with a small letter 
+- and  should return the new modified string !
+
+Example: helloWorld --> hello world'''
+
+def sentence (txtInput : str):
+    if (type(txtInput) != type("Hello")):
+        print("Try again with text ")
+    else:
+        #for txtInput in range(txtInput):
+        for char in txtInput:
+            if (char.islower() == True):
+                print(char,end='')
+                #print("Test")
+            else: 
+                print('',char.lower(),end='')
+            #if (islower(rewright)
+
+        print()
+
+
+#print(type("hello"))
+sentence("helloWorld")
