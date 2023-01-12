0fd7d84d604f5c4d13d24d738b1e83065d64138e
halrasheed <haroon.arrsheed@gmail.com>
2023-01-12 14:07:07 +0300

    Function3 Lab upload

--- Function3Lab.py
+++ Function3Lab.py
@@ -1,23 +1,29 @@
 '''
 # LAB_FUNCTIONS_3
-
 ## write a function that takes a string as a parameter
 - first check that the type of the parameter is of type str
 - then, it should separates the word at any capital letter and replace it with a small letter 
 - and  should return the new modified string !
-
 Example: helloWorld --> hello world'''
 
-def reconstruct (char : str) -> str:
-    if (char.islower() == True):
-        rerun: str = (char + '')
-        return rerun
+
+
+def reconstruct (usrTxt : str) -> str:
+    cleanText:str=""
+    for char in usrTxt:
+        if  char.isupper():
+            cleanText += " "+char.lower()
+            #rerun: str = (char + '')
+            #return rerun
                 #print("Test")
-    else:
-        rerun : str = (' '+char.lower())
-        return rerun
+        else:
+            cleanText+=char
+            #rerun : str = (' '+char.lower())
+            #return rerun
             #if (islower(rewright)
+    #Testprint(cleanText)
+    return cleanText
+    
 
 usrtxt : str = str(input("Input Your Text:"))
-for char in usrtxt:
-    print(reconstruct(char), end ='')
+print(reconstruct(usrtxt))
