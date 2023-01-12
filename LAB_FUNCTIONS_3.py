be8344bc53942c55c5f1fa59be9d957aadacd6eb
HussamAltalib <hussamaltalib@outlook.com>
2023-01-12 13:12:39 +0300

    Lab Functions 3

---
+++ LAB_FUNCTIONS_3.py
@@ -0,0 +1,23 @@
+## write a function that takes a string as a parameter
+# - first check that the type of the parameter is of type str
+# - then, it should separates the word at any capital letter and replace it with a small letter 
+# - and  should return the new modified string !
+
+# Example: helloWorld --> hello world
+
+def separate(word : str):
+    result = ""
+    # if type(word) != str :
+    #     return None
+    for char in word :      
+        if char.isupper() :
+            lower=char.lower()
+            result = result + " " + lower 
+
+        else :
+            result = result + char  
+    return result
+
+
+
+print(separate("helloWorld"))
