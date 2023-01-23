1927e5eacd192d1bd59227e916a6dbdc3e1559f0
محمد الزيداني <zidani2030@gmail.com>
2023-01-23 22:01:24 +0300

    lab

--- Untitled.py
+++ Untitled.py
@@ -1,15 +1,17 @@
-def checekworld(word):
+def checekworld(word:str):
     if type(word) != str:
         return None
+    else:
 
-    temp:str=""
+        temp:str=""
 
     for char in word:
         if char.isupper():
             temp+= " "+char.lower()
         else:
             temp+=char
-        return temp
+        word=temp
+    return temp
 
 
 print(checekworld("helloWorld"))
